d1cd45532bbe16525010a10b6e76ea71e6b83e39
Utkarsh Karki <utkarsh1998@icloud.com>
2024-05-15 10:02:36 -0700

    initial setup

---
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -0,0 +1,49 @@
+package com.example.compose.rally
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.theme.RallyTheme
+
+/**
+ * This Activity recreates part of the Rally Material Study from
+ * https://material.io/design/material-studies/rally.html
+ */
+class RallyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RallyApp()
+        }
+    }
+}
+
+@Composable
+fun RallyApp() {
+    RallyTheme {
+        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        Scaffold(
+            topBar = {
+                RallyTabRow(
+                    allScreens = rallyTabRowScreens,
+                    onTabSelected = { screen -> currentScreen = screen },
+                    currentScreen = currentScreen
+                )
+            }
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {
+                currentScreen.screen()
+            }
+        }
+    }
+}
